636f27035336636b54d3dcbdab70de2e4423e94a
FanTom6699 <123177723+FanTom6699@users.noreply.github.com>
2025-09-18 00:28:37 +0500

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-TOKEN = os.getenv('6711143584:AAGDrBrQek_q4X2s_iONkQmEafuk-b6SkrM')
-DATABASE_NAME = 'beer_bot.db'
-BEER_COOLDOWN = 3 * 60 * 60 # 3 часа в секундах
-RATING_CHANGE = 10 # Максимальное изменение рейтинга
