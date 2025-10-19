b3f6ed776269e66e277687eb0ab77e381ba9bc52
FanTom6699 <123177723+FanTom6699@users.noreply.github.com>
2025-10-19 17:00:43 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,28 @@ import logging
 from aiogram import Bot, Dispatcher
 
 import config
-from handlers import router, db
+from handlers import router, admin_router, db
 
 async def main():
-    # Настройка логирования
+    # Настройка логирования для вывода информации в консоль
     logging.basicConfig(level=logging.INFO)
     
-    # Инициализация базы данных
+    # Инициализация базы данных (создание таблиц, если их нет)
     await db.initialize()
     
     # Создание объектов бота и диспетчера
     bot = Bot(token=config.BOT_TOKEN)
     dp = Dispatcher()
     
-    # Подключение роутера с хэндлерами
+    # Подключение роутеров с командами
+    # Подключаем команды для обычных пользователей
     dp.include_router(router)
+    # Подключаем команды для админа
+    dp.include_router(admin_router)
 
-    # Запуск бота
+    # Перед запуском polling удаляем все старые обновления, чтобы бот не отвечал на старые сообщения
     await bot.delete_webhook(drop_pending_updates=True)
+    # Запуск бота
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
