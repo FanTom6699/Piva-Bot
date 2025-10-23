d69bffb13a32b2e51b3cb8480b05b700842c0ddb
FanTom6699 <123177723+FanTom6699@users.noreply.github.com>
2025-10-23 21:20:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,25 +5,24 @@ import logging
 from aiogram import Bot, Dispatcher
 
 import config
-from handlers import main_router # <-- ИЗМЕНЕНИЕ ЗДЕСЬ
-from database import Database # <-- Нам нужен класс, а не объект
+from handlers import main_router
+from database import Database
+from settings import settings_manager # <-- ИМПОРТИРУЕМ МЕНЕДЖЕР
 
 async def main():
-    # Настройка логирования
     logging.basicConfig(level=logging.INFO)
     
-    # Инициализация базы данных
     db = Database(db_name='/data/bot_database.db')
     await db.initialize()
     
-    # Создание объектов бота и диспетчера
+    # --- ЗАГРУЖАЕМ НАСТРОЙКИ ПРИ СТАРТЕ ---
+    await settings_manager.load_settings(db)
+    
     bot = Bot(token=config.BOT_TOKEN)
     dp = Dispatcher()
     
-    # Подключение главного роутера
-    dp.include_router(main_router) # <-- ИЗМЕНЕНИЕ ЗДЕСЬ
+    dp.include_router(main_router)
 
-    # Запуск бота
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
