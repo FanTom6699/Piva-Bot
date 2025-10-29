4ef42f8f480614ad5c5e650ac39c38099249ef49
FanTom6699 <123177723+FanTom6699@users.noreply.github.com>
2025-10-30 00:16:37 +0500

    Update game_raid.py

--- handlers/game_raid.py
+++ handlers/game_raid.py
@@ -14,7 +14,7 @@ from aiogram.exceptions import TelegramBadRequest
 # ИСПРАВЛЕННЫЕ ИМПОРТЫ (добавлены ..)
 from database import Database
 from settings import SettingsManager
-from common import check_user_registered
+from .common import check_user_registered
 
 # --- ИНИЦИАЛИЗАЦИЯ ---
 raid_router = Router()
