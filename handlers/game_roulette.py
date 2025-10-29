95492027c723c49b4e531fa7fa7ad867b22c2de8
FanTom6699 <123177723+FanTom6699@users.noreply.github.com>
2025-10-30 00:12:28 +0500

    Update game_roulette.py

--- handlers/game_roulette.py
+++ handlers/game_roulette.py
@@ -12,10 +12,10 @@ from aiogram.filters.callback_data import CallbackData
 from aiogram.exceptions import TelegramBadRequest
 
 # ИСПРАВЛЕННЫЕ ИМПОРТЫ (добавлены ..)
-from ..database import Database
-from ..settings import SettingsManager
-from .common import check_user_registered
-from ..utils import format_time_delta
+from database import Database
+from settings import SettingsManager
+from common import check_user_registered
+from utils import format_time_delta
 
 # --- ИНИЦИАЛИЗАЦИЯ ---
 roulette_router = Router()
