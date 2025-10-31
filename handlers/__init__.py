fe8cbaba0525692a4202a51675a36b68da7b9ec2
FanTom6699 <123177723+FanTom6699@users.noreply.github.com>
2025-10-31 23:59:24 +0500

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,19 +1,22 @@
-# # handlers/__init__.py
+# handlers/__init__.py
 from aiogram import Router
 
-from .admin import admin_router
 from .common import common_router
-from .user_commands import user_commands_router
-from .game_ladder import ladder_router
-from .game_roulette import roulette_router
-from .game_raid import raid_router
+from .admin import admin_router
+from .game_roulette import game_router as roulette_router
+from .game_ladder import game_router as ladder_router
+# --- ИСПРАВЛЕНИЕ: 'user_commands_router' -> 'user_router' ---
+from .user_commands import user_router
+from .game_raid import game_router as raid_router
 
 main_router = Router()
+
 main_router.include_routers(
     admin_router,
     common_router,
-    user_commands_router,
-    ladder_router,
+    # --- ИСПРАВЛЕНИЕ: 'user_commands_router' -> 'user_router' ---
+    user_router,
     roulette_router,
-    raid_router
+    ladder_router,
+    raid_router,
 )
