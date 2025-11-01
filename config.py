13d1e9fe5a2119cc4502678506958ebfa09cdeb1
FanTom6699 <123177723+FanTom6699@users.noreply.github.com>
2025-11-01 22:35:30 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 # config.py
+BOT_TOKEN = "6711143584:AAGH_R-5RVrAc9OBnbyrjwhKyPjddsLQiBo"        # Твой ОСНОВНОЙ токен (Пиво, Рейды)
+BOT_TOKEN_MAFIA="8202933355:AAG__kkMUjTzDoqDk0d6A9tRLekIXgf3r9c"  # Твой НОВЫЙ токен (Мафия)
 ADMIN_ID = 5658493362
