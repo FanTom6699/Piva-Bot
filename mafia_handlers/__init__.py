a128d16ef16b313f3b200bb5d4ffa44785fa394a
FanTom6699 <123177723+FanTom6699@users.noreply.github.com>
2025-11-01 22:32:14 +0500

    Update __init__.py

--- mafia_handlers/__init__.py
+++ mafia_handlers/__init__.py
@@ -1,1 +1,12 @@
-1
+# mafia_handlers/__init__.py
+from aiogram import Router
+from .common import mafia_common_router
+
+# Сюда позже добавим .lobby.py, .game.py
+# from .lobby import mafia_lobby_router
+
+mafia_router = Router()
+mafia_router.include_routers(
+    mafia_common_router
+    # mafia_lobby_router
+)
