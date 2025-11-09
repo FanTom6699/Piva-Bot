87bc0a3ed4e0b805d9d542921cf5a907fabdaa75
FanTom6699 <123177723+FanTom6699@users.noreply.github.com>
2025-11-09 07:38:16 +0500

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -7,7 +7,9 @@ from .user_commands import user_commands_router
 from .game_ladder import ladder_router
 from .game_roulette import roulette_router
 from .game_raid import raid_router
-from .shop import shop_router # ✅ ДОБАВЛЕНО
+from .shop import shop_router
+from .farm import farm_router # ✅ ДОБАВЛЕНО
+from .give import give_router # ✅ ДОБАВЛЕНО
 
 main_router = Router()
 main_router.include_routers(
@@ -17,5 +19,7 @@ main_router.include_routers(
     ladder_router,
     roulette_router,
     raid_router,
-    shop_router # ✅ ДОБАВЛЕНО
+    shop_router,
+    farm_router, # ✅ ДОБАВЛЕНО
+    give_router  # ✅ ДОБАВЛЕНО
 )
