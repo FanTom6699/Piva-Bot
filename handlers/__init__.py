625382a0a65c1619c2df6b3b4e4c7625c18880ef
FanTom6699 <123177723+FanTom6699@users.noreply.github.com>
2025-11-11 11:39:12 +0500

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,37 +1,29 @@
 # handlers/__init__.py
 from aiogram import Router
 
-# --- Базовые Хэндлеры ---
 from .admin import admin_router
 from .common import common_router
 from .user_commands import user_commands_router
-
-# --- Хэндлеры Игр ---
 from .game_ladder import ladder_router
 from .game_roulette import roulette_router
 from .game_raid import raid_router
 
-# --- ✅ НОВЫЕ ХЭНДЛЕРЫ Фермы ---
+# ✅✅✅ НОВЫЕ СТРОКИ ✅✅✅
 from .farm import farm_router
-# (Убираем ошибочный импорт farm_updater)
-from .give import give_router
-from .shop import shop_router # ✅ Включаем Магазин
+from .shop import shop_router
+# ✅✅✅ КОНЕЦ НОВЫХ СТРОК ✅✅✅
 
 main_router = Router()
 main_router.include_routers(
     admin_router,
     common_router,
-    
-    # --- Игры ---
+    user_commands_router,
     ladder_router,
     roulette_router,
     raid_router,
     
-    # --- Ферма ---
-    farm_router,   # ✅ Включаем Ферму
-    give_router,
-    shop_router,   # ✅ Включаем Магазин
-    
-    # --- Команды юзера должны быть последними ---
-    user_commands_router
+    # ✅✅✅ НОВЫЕ СТРОКИ ✅✅✅
+    farm_router,
+    shop_router
+    # ✅✅✅ КОНЕЦ НОВЫХ СТРОК ✅✅✅
 )
