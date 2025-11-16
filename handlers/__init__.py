4056edea990e9c5ecb1d41c361ec5d07e0ecf0b5
FanTom6699 <123177723+FanTom6699@users.noreply.github.com>
2025-11-16 20:05:14 +0500

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -9,8 +9,11 @@ from .game_roulette import roulette_router
 from .game_raid import raid_router
 from .farm import farm_router # (Ферма)
 # from .shop import shop_router # (Этот мы отключили в прошлый раз)
-from .game_mafia_lobby import mafia_lobby_router
-from .game_mafia_core import mafia_game_router
+
+# --- ✅ (Piva Bot) ВРЕМЕННО ОТКЛЮЧЕНО ---
+# from .game_mafia_lobby import mafia_lobby_router
+# from .game_mafia_core import mafia_game_router
+# --- ---
 
 main_router = Router()
 main_router.include_routers(
@@ -22,12 +25,11 @@ main_router.include_routers(
     raid_router,
     farm_router, # (Ферма)
     # shop_router, # (Этот мы отключили в прошлый раз)
-    mafia_lobby_router,
-    mafia_game_router
+    
+    # --- ✅ (Piva Bot) ВРЕМЕННО ОТКЛЮЧЕНО ---
+    # mafia_lobby_router,
+    # mafia_game_router
+    # --- ---
 )
 
-# ---
-# (Piva Bot) Я УДАЛИЛ отсюда 'farm_updater_router'. 
-# Он не должен здесь подключаться.
-# Он запускается только из main.py.
-# ---
+# (farm_updater_router мы убрали в прошлый раз, и это ПРАВИЛЬНО)
