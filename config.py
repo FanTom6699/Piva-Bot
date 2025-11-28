9ed0b69263c4e4fbe5b977258b8dc34080f7d9a7
FanTom6699 <123177723+FanTom6699@users.noreply.github.com>
2025-11-28 22:38:51 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # config.py
-BOT_TOKEN = "6711143584:AAGH_R-5RVrAc9OBnbyrjwhKyPjddsLQiBo"        # Твой ОСНОВНОЙ токен (Пиво, Рейды)
+BOT_TOKEN = "6711143584:AAH7KyGIQdNFYM6d6x6Iql8PNn25mcXlj7w"        # Твой ОСНОВНОЙ токен (Пиво, Рейды)
 ADMIN_ID = 5658493362
